f04e6b5b0b8daaed36e7eabd45071e5ac85f4146
Brandon Martinez <1brandonmartinez1@gmail.com>
2022-05-27 11:56:52 -0500

    Adds rigidbody cube and UpArrow functionality

--- Assets/Scripts/CubeScript.cs
+++ Assets/Scripts/CubeScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
+    Rigidbody rbCube;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Assigns Rigidbody to cube
+        rbCube = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Gives UpArrow "jumping" functionality for cube
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rbCube.AddForce(Vector3.up * 500);
+        }
     }
 }
