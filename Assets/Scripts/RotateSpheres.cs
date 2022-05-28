a5ede0068c2b3616431f04d4a6b159fc5de7d980
Brandon Martinez <1brandonmartinez1@gmail.com>
2022-05-28 18:47:17 -0500

    Adds rotation to RotateSpheres.cs and sphere prefab

--- Assets/Scripts/RotateSpheres.cs
+++ Assets/Scripts/RotateSpheres.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//can find documentation w/ monobehavior
 public class RotateSpheres : MonoBehaviour
 {
+    Vector3 movement;
+
+    // "public" allows these vars to be seen/modified in Unity
+    public int xi, yi, zi;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        movement = new Vector3(xi, yi, zi);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Time.deltaTime allows the movement to be smooth
+        transform.Rotate(movement * Time.deltaTime);
     }
 }
